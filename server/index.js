03ee146f6d46c4e476eaac01e8629184fe5a63e2
TannerFrancis <30556094+Financ3@users.noreply.github.com>
2021-04-30 10:04:12 -0500

    set up basic massive/express server including .env file

---
+++ server/index.js
@@ -0,0 +1,40 @@
+//root server file
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env')});
+const massive = require('massive');
+const session = require('express-session');
+const express = require('express');
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
+
+const app = express();
+app.use(express.json());
+
+//Controller endpoints here
+
+
+
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 525600
+    }
+}))
+
+
+//establish the database connection and start the server
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}, {scripts: path.join(__dirname, '../db')})
+.then(dbInstance => {
+    app.set('db', dbInstance);
+    app.listen(SERVER_PORT, () => {
+        console.log(`Server running on port ${SERVER_PORT}`)
+    });
+}).catch(err=>console.log(err));
