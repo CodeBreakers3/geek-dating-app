a728567a1ea6fb489e4a73636ec7bfd947d4f2ac
Jared Andersen <j-mac@Jareds-MacBook-Pro.local>
2021-05-05 14:17:41 -0700

    end of day 5/5

--- server/index.js
+++ server/index.js
@@ -1,79 +1,82 @@
 //root server file
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env')});
-const massive = require('massive');
-const session = require('express-session');
-const express = require('express');
-const profileController = require('./controllers/profileController');
-const matchesController = require('./controllers/matchesController');
-const chatsController = require('./controllers/chatsController');
-const authCtrl = require('./controllers/authController');
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const massive = require("massive");
+const session = require("express-session");
+const express = require("express");
+const profileController = require("./controllers/profileController");
+const matchesController = require("./controllers/matchesController");
+const chatsController = require("./controllers/chatsController");
+const authCtrl = require("./controllers/authController");
 
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
 
 const app = express();
 app.use(express.json());
 
-app.use(session({
+app.use(
+  session({
     secret: SESSION_SECRET,
     resave: true,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1000 * 60 * 525600
-    }
-}))
-
+      maxAge: 1000 * 60 * 525600,
+    },
+  })
+);
 
 //Controller endpoints here
 
 //PROFILE ENDPOINTS
-    //update profile: receives a profile object and sends it to the DB to update that profile object in the DB. 
-    app.put('/api/updateprofile/', profileController.updateProfile);
+//update profile: receives a profile object and sends it to the DB to update that profile object in the DB.
+app.get("/api/getprofile/:profile_id", profileController.getProfile);
+app.put("/api/updateprofile/:profile_id", profileController.updateProfile);
+app.post("/api/createprofile", profileController.createProfile);
+app.delete("/api/deleteprofile/:profile_id", profileController.deleteProfile);
 
 //MATCHES ENDPOINTS
-    //LIKE: add to the like table, check for match on like table, if matched, add match in the match table. 
-    //The front end should send the liking users ID as a parameter in the URL. The liked users profile ID will be in the query string variables ?profile_id=X.  
+//LIKE: add to the like table, check for match on like table, if matched, add match in the match table.
+//The front end should send the liking users ID as a parameter in the URL. The liked users profile ID will be in the query string variables ?profile_id=X.
 
-    //endpoint will return the match ID if there was a match, a boolean "false" if there was no match. Either way the like will be logged in the database.
-    app.post('/api/like/:user_id', matchesController.like);
+//endpoint will return the match ID if there was a match, a boolean "false" if there was no match. Either way the like will be logged in the database.
+app.post("/api/like/:user_id", matchesController.like);
 
-    //DISLIKE: Create a bogus update for the unliked profile on the profile table*
-    //The front end should send the id for the unliked profile in the URL parameters
-    app.put('/api/dislike/:profile_id', matchesController.dislike);
+//DISLIKE: Create a bogus update for the unliked profile on the profile table*
+//The front end should send the id for the unliked profile in the URL parameters
+app.put("/api/dislike/:profile_id", matchesController.dislike);
 
-    //UNMATCH: *Remove match from matches table, remove like from likes table, Create a bogus update for the unmatch profile on the profile table**
-    //front end should send the match ID in the url params. The unmatching user_id in the query string, and the unmatched profile_id in the query string. 
-    //?user_id=X&profile_id=Y
-    app.delete('/api/unmatch/:match_id', matchesController.unmatch);
-    
-    
-    app.get(`/api/chat`, chatsController.getChats);
-    app.post(`/api/chat`, chatsController.addChatReply);
-    app.put('/api/chat/:chat_id', chatsController.updateChatReply);
-    app.delete('/api/chat/:chat_id', chatsController.deleteChatReply);
+//UNMATCH: *Remove match from matches table, remove like from likes table, Create a bogus update for the unmatch profile on the profile table**
+//front end should send the match ID in the url params. The unmatching user_id in the query string, and the unmatched profile_id in the query string.
+//?user_id=X&profile_id=Y
+app.delete("/api/unmatch/:match_id", matchesController.unmatch);
 
-    // app.post(`/auth/register`, authController.login);
+app.get(`/api/chat`, chatsController.getChats);
+app.post(`/api/chat`, chatsController.addChatReply);
+app.put("/api/chat/:chat_id", chatsController.updateChatReply);
+app.delete("/api/chat/:chat_id", chatsController.deleteChatReply);
+
+// app.post(`/auth/register`, authController.login);
 
 // Authentication Controller Endpoints
-app.post("/auth/signup",  authCtrl.register)
-app.post("/auth/login", authCtrl.login)
-app.delete("/auth/logout", authCtrl.logOut)
-app.put("/auth/updateuser/:id", authCtrl.updateUser)
-
-
-
+app.post("/auth/signup", authCtrl.register);
+app.post("/auth/login", authCtrl.login);
+app.delete("/auth/logout", authCtrl.logOut);
+app.put("/auth/updateuser/:id", authCtrl.updateUser);
 
 //establish the database connection and start the server
-massive({
+massive(
+  {
     connectionString: CONNECTION_STRING,
     ssl: {
-        rejectUnauthorized: false
-    }
-}, {scripts: path.join(__dirname, '../db')})
-.then(dbInstance => {
-    app.set('db', dbInstance);
+      rejectUnauthorized: false,
+    },
+  },
+  { scripts: path.join(__dirname, "../db") }
+)
+  .then((dbInstance) => {
+    app.set("db", dbInstance);
     app.listen(SERVER_PORT, () => {
-        console.log(`Server running on port ${SERVER_PORT}`)
+      console.log(`Server running on port ${SERVER_PORT}`);
     });
->>>>>>> main
-}).catch(err=>console.log(err));
+  })
+  .catch((err) => console.log(err));
--- src/Components/inAppRoutes/Matches/Chats.js
+++ src/Components/inAppRoutes/Matches/Chats.js
@@ -1,72 +1,72 @@
-import React, {useState, useEffect, useRef} from 'react'
-import io from 'socket.io-client';
+// import React, {useState, useEffect, useRef} from 'react'
+// import io from 'socket.io-client';
 
-const Chat = ()=> {
-    const [yourID, setYourID] = useState();
-        const [messages,setMessages] = useState([]);
-        const [message, setMessage] = useState('');
+// const Chat = ()=> {
+//     const [yourID, setYourID] = useState();
+//         const [messages,setMessages] = useState([]);
+//         const [message, setMessage] = useState('');
 
-    const socketRef = useRef()
+//     const socketRef = useRef()
 
-useEffect(()=>{
-    socketRef.current = io.connect('http://localhost:3111');
+// useEffect(()=>{
+//     socketRef.current = io.connect('http://localhost:3111');
 
-    socketRef.current.on('your id', id =>{
-        setYourID(id)
-    })
+//     socketRef.current.on('your id', id =>{
+//         setYourID(id)
+//     })
 
-socketRef.current.on('message', (message)=>{
-recievedMessage(message)
-})
+// socketRef.current.on('message', (message)=>{
+// recievedMessage(message)
+// })
 
-})
-function recievedMessage(message){
-    setMessages(oldMessages => [...oldMessages,message])
-}
-function sendMessage(e){
-    e.preventDefault()
-    const messageObject ={
-        body: message,
-        id:yourID,
-    }
-    setMessage('')
-socketRef.current.emit('send message', messageObject)
-}
+// })
+// function recievedMessage(message){
+//     setMessages(oldMessages => [...oldMessages,message])
+// }
+// function sendMessage(e){
+//     e.preventDefault()
+//     const messageObject ={
+//         body: message,
+//         id:yourID,
+//     }
+//     setMessage('')
+// socketRef.current.emit('send message', messageObject)
+// }
 
-function handleChange(e){
-setMessage(e.target.value)
-}
+// function handleChange(e){
+// setMessage(e.target.value)
+// }
 
 
-    return(
-    <div>
-        <section>
-{messages.map((message,i)=> {
-    if(message.id === yourID){
-        return(
-            <div key={i}>
-                <div>
-                {message.body}
-                </div>
-            </div>
-        )
-    }
-    return(
-        <div key={i}>
-<div>
-    {message.body}
-</div>
-        </div>
-    )
-})}
-        </section>
-        <form onSubmit={sendMessage} >
-<textarea value={message} onChange={handleChange} placeholder='LEEROY JENKINS!'>
+//     return(
+//     <div>
+//         <section>
+// {messages.map((message,i)=> {
+//     if(message.id === yourID){
+//         return(
+//             <div key={i}>
+//                 <div>
+//                 {message.body}
+//                 </div>
+//             </div>
+//         )
+//     }
+//     return(
+//         <div key={i}>
+// <div>
+//     {message.body}
+// </div>
+//         </div>
+//     )
+// })}
+//         </section>
+//         <form onSubmit={sendMessage} >
+// <textarea value={message} onChange={handleChange} placeholder='LEEROY JENKINS!'>
    
-</textarea> 
-<button>send</button>
-        </form>
-    </div>
-    )
-}
-export default Chat
+// </textarea> 
+// <button>send</button>
+//         </form>
+//     </div>
+//     )
+// }
+// export default Chat
