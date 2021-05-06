fc2056122085ba6ae809fede8fc8d32cb123c111
TannerFrancis <30556094+Financ3@users.noreply.github.com>
2021-05-06 16:09:21 -0500

    verifying up to date

--- src/Components/inAppRoutes/Matches/Matches.js
+++ src/Components/inAppRoutes/Matches/Matches.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import axios from 'axios'
 import { connect } from "react-redux";
 import { getMatches,getMatchedChat } from "../../../ducks/profileReducer";
-import "../../../reset.css";
 import "./matches.css";
 
 const Matches = (props) => {
