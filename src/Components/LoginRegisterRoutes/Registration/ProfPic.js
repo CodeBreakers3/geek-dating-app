da415eb6ae7a2766ae1901eecb15c5493a813a38
TannerFrancis <30556094+Financ3@users.noreply.github.com>
2021-05-13 15:04:31 -0500

    finished css for prof pics

--- src/Components/LoginRegisterRoutes/Registration/ProfPic.js
+++ src/Components/LoginRegisterRoutes/Registration/ProfPic.js
@@ -1,3 +1,4 @@
+import "./CSS/ProfPic.css"
 import React, {useState} from 'react'
 import BackButton2 from '../BackButton2'
 
@@ -34,32 +35,68 @@ function ProfPic(props) {
     return (
         <div className="profpic-view-wrapper-container">
             <BackButton2 setIndexTracker={props.setIndexTracker} indexTracker={props.indexTracker}/>
-            <div>
-                <p>Upload some photos!</p>
+
+            <div className="profpic-view-title">
+                <h1>Profile Pictures</h1>
             </div>
+
             <div className="profpic-view-picture-container-main">
-
-                {!profilePic1?<img src="https://via.placeholder.com/150" alt="placeholder"/>:<img src={profilePic1} alt="user-picture-1"/>}
+                <p>Main Profile Pic:</p>
+                <div className="profpic-view-image-container">
+                    {!profilePic1?<img src="https://via.placeholder.com/150" alt="placeholder"/>:<img src={profilePic1} alt="user-picture-1"/>}
+                </div>
+                <p>Image URL:</p>
                 <input type="text" defaultValue={props.actualProfile.photo_one} onChange={(e) => setprofilePic1(e.target.value)}></input>
-                {!profilePic2?<img src="https://via.placeholder.com/150" alt="placeholder"/>:<img src={profilePic2} alt="user-picture-1"/>}
-                <input type="text" defaultValue={props.actualProfile.photo_two} onChange={(e) => setprofilePic2(e.target.value)}></input>
-                {!profilePic3?<img src="https://via.placeholder.com/150" alt="placeholder"/>:<img src={profilePic3} alt="user-picture-1"/>}
-                <input type="text" defaultValue={props.actualProfile.photo_three} onChange={(e) => setprofilePic3(e.target.value)}></input>
-                {!profilePic4?<img src="https://via.placeholder.com/150" alt="placeholder"/>:<img src={profilePic4} alt="user-picture-1"/>}
-                <input type="text" defaultValue={props.actualProfile.photo_four} onChange={(e) => setprofilePic4(e.target.value)}></input>
-                
-
                 
             </div>
 
             <div className="profpic-view-picture-container-sub">
+                <p>Additional Pics:</p>
+                <div className="profpic-view-picture-container-sub-flex">
+                    <div className="profpic-view-picture-input-container">
+                        <div className="profpic-view-image-container">
+                            {!profilePic2?
+                                <img src="https://via.placeholder.com/150" alt="placeholder"/>:
+                                <img src={profilePic2} alt="user-picture-1"/>}
+                        </div>
+                        <p>Image URL:</p>
+                        <input type="text" defaultValue={props.actualProfile.photo_two} onChange={(e) => setprofilePic2(e.target.value)}></input>
+                    </div>
 
-                {!profilePic5?<img src="https://via.placeholder.com/150" alt="placeholder"/>:<img src={profilePic5} alt="user-picture-1"/>}
-                <input type="text" defaultValue={props.actualProfile.photo_five} onChange={(e) => setprofilePic5(e.target.value)}></input>
+                    <div className="profpic-view-picture-input-container">
+                        <div className="profpic-view-image-container">
+                            {!profilePic3?
+                                <img src="https://via.placeholder.com/150" alt="placeholder"/>:
+                                <img src={profilePic3} alt="user-picture-1"/>}
+                        </div>
+                        <p>Image URL:</p>
+                        <input type="text" defaultValue={props.actualProfile.photo_three} onChange={(e) => setprofilePic3(e.target.value)}></input>
+                    </div>
+
+                    <div className="profpic-view-picture-input-container">
+                        <div className="profpic-view-image-container">
+                            {!profilePic4?
+                                <img src="https://via.placeholder.com/150" alt="placeholder"/>:
+                                <img src={profilePic4} alt="user-picture-1"/>}
+                        </div>
+                        <p>Image URL:</p>
+                        <input type="text" defaultValue={props.actualProfile.photo_four} onChange={(e) => setprofilePic4(e.target.value)}></input>
+                    </div>
+
+                    <div className="profpic-view-picture-input-container">
+                        <div className="profpic-view-image-container">
+                            {!profilePic5?
+                                <img src="https://via.placeholder.com/150" alt="placeholder"/>:
+                                <img src={profilePic5} alt="user-picture-1"/>}
+                        </div>
+                        <p>Image URL:</p>
+                        <input type="text" defaultValue={props.actualProfile.photo_five} onChange={(e) => setprofilePic5(e.target.value)}></input>
+                    </div>
+                </div>
             </div>
 
             <div className="profpic-view-save-button">
-                <button onClick={() => nextView()}>Save and Continue</button>
+                <div onClick={() => nextView()}>Save and Continue</div>
             </div>
                 
         </div>
